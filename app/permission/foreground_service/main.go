c525c77eb0c7c3ac47b7b7ab042aaa687664c11e
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-01-22 13:46:12 -0500

    add missing permissions

---
+++ app/permission/foreground_service/main.go
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
+/*
+Package foreground service implements permissions to access service.
+
+# Android
+
+The following entries will be added to AndroidManifest.xml:
+
+	<uses-permission android:name="android.permission.FOREGROUND_SERVICE"/>
+*/
+package foreground_service
---
+++ app/permission/post_notifications/main.go
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
+/*
+Package post_notifications implements permissions to access post notifications.
+
+# Android
+
+The following entries will be added to AndroidManifest.xml:
+
+	<uses-permission android:name="android.permission.POST_NOTIFICATIONS"/>
+*/
+package post_notifications
